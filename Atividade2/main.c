9b2eb6170fc04e4b1ea1e7bf8236e93ca632b3a8
Alexandre A. Scrocaro Junior <37521313+alescrocaro@users.noreply.github.com>
2022-05-23 20:47:46 -0300

    Add files via upload

---
+++ Atividade2/main.c
@@ -0,0 +1,126 @@
+#include <GL/glut.h>
+
+int width, height;
+float spin = 0;
+
+void Timer(int value);
+void display(void);
+void reshape(int w, int h);
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+  glutInitWindowSize(800, 800);
+  glutInitWindowPosition(500, 200);
+  glutCreateWindow("Bule - Quatro projecoes");
+
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+
+  glutDisplayFunc(display);
+
+  //glutTimerFunc(2,Timer, 1);
+
+  glutReshapeFunc(reshape);
+
+  glutMainLoop();
+}
+
+// função de callback de glutReshapeFunc
+void reshape(int w, int h) {
+  width = w;
+  height = h;
+}
+
+// função de callback de glutTimerFunc
+void Timer(int value){
+  spin += 33.0f;
+  glutPostRedisplay();
+  glutTimerFunc(33, Timer, 1);
+}
+
+
+
+void display(void) {
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);       // define a cor de fundo
+  glClear(GL_COLOR_BUFFER_BIT);
+
+
+  // -=-=-=-=-=- Viewport canto superior esquerdo -=-=-=-=-=-
+  glViewport(0, height/2, width/2, height/2);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  glOrtho(-3, 3, -3, 3, 1, 50);               // define uma projecao ortogonal
+  gluLookAt(0, 3, 0,
+            0, 0, 0,
+            0,  0,-3);
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  glutWireTeapot(2);                          // render a wireframe teapot
+
+  glPopMatrix();                              // restaura a matriz anterior
+
+
+
+  // -=-=-=-=-=- Viewport canto superior direito -=-=-=-=-=-
+  glViewport(width/2, height/2, width/2, height/2);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  glOrtho(-3, 3, -3, 3, 1, 50);
+  gluLookAt(-3, 0, 0,
+            0, 0, 0,
+            0, 1, 0);
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  glutWireTeapot(2);
+
+  glPopMatrix();
+
+
+
+  // -=-=-=-=-=- Viewport canto inferior esquerdo -=-=-=-=-=-
+  glViewport(0, 0, width/2, height/2);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  glOrtho(-3, 3, -3, 3, 1, 50);
+  gluLookAt(0, 0, 3,
+            0, 0, 0,
+            0, 1, 0);
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  glutWireTeapot(2);
+
+  glPopMatrix();
+
+
+
+  // -=-=-=-=-=- Viewport canto inferior direito -=-=-=-=-=-
+  glViewport(width/2, 0, width/2, height/2);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  gluPerspective(70, 1, 1, 50);               // define uma projecao perspectiva
+  gluLookAt(-3, 0, 3.5,
+            0, 0, 0,
+            0, 1, 0);
+
+  glRotatef(45, 1, 0, 0);                     // rotaciona o objeto
+  glRotatef(spin, 0, 0, 1);                 // rotaciona o objeto
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  glutWireTeapot(2);
+
+  glPopMatrix();
+
+
+
+  glFlush();
+}
+
